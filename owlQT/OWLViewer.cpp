d4d358f1bb6c4780ba715762ffa5118a77e4dab8
Ingo Wald <ingowald@gmail.com>
2020-11-14 11:50:18 -0700

    imported all camera handling, events not yet hooked up

--- owlQT/OWLViewer.cpp
+++ owlQT/OWLViewer.cpp
@@ -22,7 +22,105 @@
 #include <QApplication>
 #include <QDesktopWidget>
 
+#include "InspectMode.h"
+#include "FlyMode.h"
+#include <sstream>
+
+// eventually to go into 'apps/'
+#define STB_IMAGE_WRITE_IMPLEMENTATION 1
+#include "samples/common/3rdParty/stb/stb_image_write.h"
+
 namespace owlQT {
+
+    inline const char* getGLErrorString( GLenum error )
+{
+    switch( error )
+    {
+        case GL_NO_ERROR:            return "No error";
+        case GL_INVALID_ENUM:        return "Invalid enum";
+        case GL_INVALID_VALUE:       return "Invalid value";
+        case GL_INVALID_OPERATION:   return "Invalid operation";
+        //case GL_STACK_OVERFLOW:      return "Stack overflow";
+        //case GL_STACK_UNDERFLOW:     return "Stack underflow";
+        case GL_OUT_OF_MEMORY:       return "Out of memory";
+        //case GL_TABLE_TOO_LARGE:     return "Table too large";
+        default:                     return "Unknown GL error";
+    }
+}
+
+
+
+#define DO_GL_CHECK
+#ifdef DO_GL_CHECK
+#    define GL_CHECK( call )                                                   \
+        do                                                                     \
+        {                                                                      \
+            call;                                                              \
+            GLenum err = glGetError();                                         \
+            if( err != GL_NO_ERROR )                                           \
+            {                                                                  \
+                std::stringstream ss;                                          \
+                ss << "GL error " <<  getGLErrorString( err ) << " at " \
+                   << __FILE__  << "(" <<  __LINE__  << "): " << #call         \
+                   << std::endl;                                               \
+                std::cerr << ss.str() << std::endl;                            \
+                throw std::runtime_error( ss.str().c_str() );                        \
+            }                                                                  \
+        }                                                                      \
+        while (0)
+
+
+#    define GL_CHECK_ERRORS( )                                                 \
+        do                                                                     \
+        {                                                                      \
+            GLenum err = glGetError();                                         \
+            if( err != GL_NO_ERROR )                                           \
+            {                                                                  \
+                std::stringstream ss;                                          \
+                ss << "GL error " <<  getGLErrorString( err ) << " at " \
+                   << __FILE__  << "(" <<  __LINE__  << ")";                   \
+                std::cerr << ss.str() << std::endl;                            \
+                throw std::runtime_error( ss.str().c_str() );                    \
+            }                                                                  \
+        }                                                                      \
+        while (0)
+
+#else
+#    define GL_CHECK( call )   do { call; } while(0)
+#    define GL_CHECK_ERRORS( ) do { ;     } while(0)
+#endif
+
+
+
+  OWLViewer::OWLViewer(const std::string &title,
+                       const vec2i &initWindowSize,
+                       bool visible)
+  {
+    // glfwSetErrorCallback(glfw_error_callback);
+    // // glfwInitHint(GLFW_COCOA_MENUBAR, GLFW_FALSE);
+
+    // initGLFW();
+      
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // glfwWindowHint(GLFW_VISIBLE, visible);
+      
+    // handle = glfwCreateWindow(initWindowSize.x, initWindowSize.y,
+    //                           title.c_str(), NULL, NULL);
+    // if (!handle) {
+    //   glfwTerminate();
+    //   exit(EXIT_FAILURE);
+    // }
+      
+    // glfwSetWindowUserPointer(handle, this);
+    // glfwMakeContextCurrent(handle);
+    // glfwSwapInterval( 1 );
+    connect(&timer, SIGNAL(timeout()), this, SLOT(update()));
+    timer.start(1);
+  }
+
+
+
   OWLViewer::~OWLViewer()
   {
   
@@ -198,4 +296,332 @@ namespace owlQT {
   // vbo.bind();
   // vbo.allocate(vertData.constData(), vertData.count() * sizeof(GLfloat));
   // }
+
+
+
+
+    /*! helper function that dumps the current frame buffer in a png
+        file of given name */
+    void OWLViewer::screenShot(const std::string &fileName)
+    {
+      const uint32_t *fb
+        = (const uint32_t*)fbPointer;
+      
+      std::vector<uint32_t> pixels;
+      for (int y=0;y<fbSize.y;y++) {
+        const uint32_t *line = fb + (fbSize.y-1-y)*fbSize.x;
+        for (int x=0;x<fbSize.x;x++) {
+          pixels.push_back(line[x] | (0xff << 24));
+        }
+      }
+      stbi_write_png(fileName.c_str(),fbSize.x,fbSize.y,4,
+                     pixels.data(),fbSize.x*sizeof(uint32_t));
+      std::cout << "#owl.viewer: frame buffer written to " << fileName << std::endl;
+    }
+      
+    vec2i OWLViewer::getScreenSize()
+    {
+      // initGLFW();
+      // int numModes = 0;
+      // auto monitor = glfwGetPrimaryMonitor();
+      // if (!monitor) 
+      //   throw std::runtime_error("could not query monitor...");
+      // const GLFWvidmode *modes
+      //   = glfwGetVideoModes(monitor, &numModes);
+      // vec2i size(0,0);
+      // for (int i=0; i<numModes; i++) 
+      //   size = max(size,vec2i(modes[i].width,modes[i].height));
+      // return size;
+
+      QRect rec = QApplication::desktop()->screenGeometry();
+      int height = rec.height();
+      int width = rec.width();
+      return {width, height};
+    }
+ 
+    float computeStableEpsilon(float f)
+    {
+      return abs(f) * float(1./(1<<21));
+    }
+
+    float computeStableEpsilon(const vec3f v)
+    {
+      return max(max(computeStableEpsilon(v.x),
+                     computeStableEpsilon(v.y)),
+                 computeStableEpsilon(v.z));
+    }
+    
+  SimpleCamera::SimpleCamera(const Camera &camera)
+    {
+      auto &easy = *this;
+      easy.lens.center = camera.position;
+      easy.lens.radius = 0.f;
+      easy.lens.du     = camera.frame.vx;
+      easy.lens.dv     = camera.frame.vy;
+
+      const float minFocalDistance
+        = max(computeStableEpsilon(camera.position),
+              computeStableEpsilon(camera.frame.vx));
+
+      /*
+        tan(fov/2) = (height/2) / dist
+        -> height = 2*tan(fov/2)*dist
+      */
+      float screen_height
+        = 2.f*tanf(camera.fovyInDegrees/2.f * (float)M_PI/180.f)
+        * max(minFocalDistance,camera.focalDistance);
+      easy.screen.vertical   = screen_height * camera.frame.vy;
+      easy.screen.horizontal = screen_height * camera.aspect * camera.frame.vx;
+      easy.screen.lower_left
+        = //easy.lens.center
+        /* NEGATIVE z axis! */
+        - max(minFocalDistance,camera.focalDistance) * camera.frame.vz
+        - 0.5f * easy.screen.vertical
+        - 0.5f * easy.screen.horizontal;
+      // easy.lastModified = getCurrentTime();
+    }
+    
+    // ==================================================================
+    // actual viewerwidget class
+    // ==================================================================
+
+    void OWLViewer::resize(const vec2i &newSize)
+    {
+      // glfwMakeContextCurrent(handle);
+      if (fbPointer)
+        cudaFree(fbPointer);
+      cudaMallocManaged(&fbPointer,newSize.x*newSize.y*sizeof(uint32_t));
+      
+      fbSize = newSize;
+      // bool firstResize = false;
+      if (fbTexture == 0) {
+        GL_CHECK(glGenTextures(1, &fbTexture));
+        // firstResize = true;
+      }
+      else {
+        if (cuDisplayTexture) {
+          cudaGraphicsUnregisterResource(cuDisplayTexture);
+          cuDisplayTexture = 0;
+        }
+      }
+
+      GL_CHECK(glBindTexture(GL_TEXTURE_2D, fbTexture));
+      // GL_CHECK(glActiveTexture(GL_TEXTURE0));
+      GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, newSize.x, newSize.y, 0, GL_RGBA,
+                            GL_UNSIGNED_BYTE, nullptr));
+      
+      // We need to re-register when resizing the texture
+      cudaError_t rc = cudaGraphicsGLRegisterImage
+        (&cuDisplayTexture, fbTexture, GL_TEXTURE_2D, 0);
+      
+      // if (firstResize || !firstResize && resourceSharingSuccessful) {
+      //   const char *forceSlowDisplay = getenv("OWL_NO_CUDA_RESOURCE_SHARING");
+      bool forceSlowDisplay = false;
+      if (rc != cudaSuccess || forceSlowDisplay) {
+        std::cout << OWL_TERMINAL_RED
+                  << "Warning: Could not do CUDA graphics resource sharing "
+                  << "for the display buffer texture ("
+                  << cudaGetErrorString(cudaGetLastError())
+                  << ")... falling back to slower path"
+                  << OWL_TERMINAL_DEFAULT
+                  << std::endl;
+        resourceSharingSuccessful = false;
+        if (cuDisplayTexture) {
+          cudaGraphicsUnregisterResource(cuDisplayTexture);
+          cuDisplayTexture = 0;
+        }
+      } else {
+        resourceSharingSuccessful = true;
+      }
+      // }
+      // if (cuDisplayTexture != 0) {
+      //   cudaGraphicsUnmapResources(1, &cuDisplayTexture);
+      //   /* drop last error: */cudaGetLastError();
+      //   cuDisplayTexture = 0;
+      // }
+      // } else {
+      // if (resourceSharingSuccessful)
+      //   cudaGraphicsUnmapResources(1, &cuDisplayTexture);
+      //   resourceSharingSuccessful = true;
+      // }
+    }
+
+    
+    /*! re-draw the current frame. This function itself isn't
+      virtual, but it calls the framebuffer's render(), which
+      is */
+    void OWLViewer::draw()
+    {
+      // glfwMakeContextCurrent(handle);
+      if (resourceSharingSuccessful) {
+        GL_CHECK(cudaGraphicsMapResources(1, &cuDisplayTexture));
+        
+        cudaArray_t array;
+        GL_CHECK(cudaGraphicsSubResourceGetMappedArray(&array, cuDisplayTexture, 0, 0));
+        {
+          cudaMemcpy2DToArray(array,
+                              0,
+                              0,
+                              reinterpret_cast<const void *>(fbPointer),
+                              fbSize.x * sizeof(uint32_t),
+                              fbSize.x * sizeof(uint32_t),
+                              fbSize.y,
+                              cudaMemcpyDeviceToDevice);
+        }
+      } else {
+        GL_CHECK(glBindTexture(GL_TEXTURE_2D, fbTexture));
+        glEnable(GL_TEXTURE_2D);
+        GL_CHECK(glTexSubImage2D(GL_TEXTURE_2D,0,
+                                 0,0,
+                                 fbSize.x, fbSize.y,
+                                 GL_RGBA, GL_UNSIGNED_BYTE, fbPointer));
+      }
+      
+      glDisable(GL_LIGHTING);
+      glColor3f(1, 1, 1);
+
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+
+      glEnable(GL_TEXTURE_2D);
+      glBindTexture(GL_TEXTURE_2D, fbTexture);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      
+      glDisable(GL_DEPTH_TEST);
+
+      glViewport(0, 0, fbSize.x, fbSize.y);
+
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      glOrtho(0.f, (float)fbSize.x, 0.f, (float)fbSize.y, -1.f, 1.f);
+
+      glBegin(GL_QUADS);
+      {
+        glTexCoord2f(0.f, 0.f);
+        glVertex3f(0.f, 0.f, 0.f);
+      
+        glTexCoord2f(0.f, 1.f);
+        glVertex3f(0.f, (float)fbSize.y, 0.f);
+      
+        glTexCoord2f(1.f, 1.f);
+        glVertex3f((float)fbSize.x, (float)fbSize.y, 0.f);
+      
+        glTexCoord2f(1.f, 0.f);
+        glVertex3f((float)fbSize.x, 0.f, 0.f);
+      }
+      glEnd();
+      if (resourceSharingSuccessful) {
+        GL_CHECK(cudaGraphicsUnmapResources(1, &cuDisplayTexture));
+      }
+    }
+
+    /*! re-computes the 'camera' from the 'cameracontrol', and notify
+      app that the camera got changed */
+    void OWLViewer::updateCamera()
+    {
+      // camera.digestInto(simpleCamera);
+      // if (isActive)
+      camera.lastModified = getCurrentTime();
+    }
+
+    void OWLViewer::enableInspectMode(RotateMode rm,
+                                      const box3f &validPoiRange,
+                                      float minPoiDist,
+                                      float maxPoiDist)
+    {
+      inspectModeManipulator
+        = std::make_shared<CameraInspectMode>
+        (this,validPoiRange,minPoiDist,maxPoiDist,
+         rm==POI? CameraInspectMode::POI: CameraInspectMode::Arcball);
+      // if (!cameraManipulator)
+      cameraManipulator = inspectModeManipulator;
+    }
+
+    void OWLViewer::enableInspectMode(const box3f &validPoiRange,
+                                      float minPoiDist,
+                                      float maxPoiDist)
+    {
+      enableInspectMode(POI,validPoiRange,minPoiDist,maxPoiDist);
+    }
+
+    void OWLViewer::enableFlyMode()
+    {
+      flyModeManipulator
+        = std::make_shared<CameraFlyMode>(this);
+      // if (!cameraManipulator)
+      cameraManipulator = flyModeManipulator;
+    }
+
+    /*! this gets called when the window determines that the mouse got
+      _moved_ to the given position */
+    void OWLViewer::mouseMotion(const vec2i &newMousePosition)
+    {
+      if (lastMousePosition != vec2i(-1)) {
+        if (leftButton.isPressed)
+          mouseDragLeft  (newMousePosition,newMousePosition-lastMousePosition);
+        if (centerButton.isPressed)
+          mouseDragCenter(newMousePosition,newMousePosition-lastMousePosition);
+        if (rightButton.isPressed)
+          mouseDragRight (newMousePosition,newMousePosition-lastMousePosition);
+      }
+      lastMousePosition = newMousePosition;
+    }
+
+    void OWLViewer::mouseDragLeft  (const vec2i &where, const vec2i &delta)
+    {
+      if (cameraManipulator) cameraManipulator->mouseDragLeft(where,delta);
+    }
+
+    /*! mouse got dragged with left button pressedn, by 'delta' pixels, at last position where */
+    void OWLViewer::mouseDragCenter(const vec2i &where, const vec2i &delta)
+    {
+      if (cameraManipulator) cameraManipulator->mouseDragCenter(where,delta);
+    }
+
+    /*! mouse got dragged with left button pressedn, by 'delta' pixels, at last position where */
+    void OWLViewer::mouseDragRight (const vec2i &where, const vec2i &delta)
+    {
+      if (cameraManipulator) cameraManipulator->mouseDragRight(where,delta);
+    }
+
+    /*! mouse button got either pressed or released at given location */
+    void OWLViewer::mouseButtonLeft  (const vec2i &where, bool pressed)
+    {
+      if (cameraManipulator) cameraManipulator->mouseButtonLeft(where,pressed);
+
+      lastMousePosition = where;
+    }
+
+    /*! mouse button got either pressed or released at given location */
+    void OWLViewer::mouseButtonCenter(const vec2i &where, bool pressed)
+    {
+      if (cameraManipulator) cameraManipulator->mouseButtonCenter(where,pressed);
+
+      lastMousePosition = where;
+    }
+
+    /*! mouse button got either pressed or released at given location */
+    void OWLViewer::mouseButtonRight (const vec2i &where, bool pressed)
+    {
+      if (cameraManipulator) cameraManipulator->mouseButtonRight(where,pressed);
+
+      lastMousePosition = where;
+    }
+
+    /*! this gets called when the user presses a key on the keyboard ... */
+    void OWLViewer::key(char key, const vec2i &where)
+    {
+      if (cameraManipulator) cameraManipulator->key(key,where);
+    }
+
+
+
+    /*! this gets called when the user presses a key on the keyboard ... */
+    void OWLViewer::special(int key, int mods, const vec2i &where)
+    {
+      if (cameraManipulator) cameraManipulator->special(key,where);
+    }
+
+  
 }
--- owlQT/OWLViewer.h
+++ owlQT/OWLViewer.h
@@ -14,34 +14,51 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
-#include "owl/common/math/box.h"
+#include "owlQT/Camera.h"
 
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 #include <QOpenGLBuffer>
 #include <QTimer>
 
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+
 QT_FORWARD_DECLARE_CLASS(QOpenGLShaderProgram);
 QT_FORWARD_DECLARE_CLASS(QOpenGLTexture)
 
 namespace owlQT {
-  using namespace owl;
-  using namespace owl::common;
   
+  /*! base abstraction for a camera that can generate rays. For this
+    viewer, we assume we're dealine with a camera that has a
+    rectangular viewing plane that's in focus, and a circular (and
+    possible single-point) lens for depth of field. At some later
+    point this should also capture time.{t0,t1} for motion blur, but
+    let's leave this out for now. */
+  struct SimpleCamera
+  {
+    inline SimpleCamera() {}
+    SimpleCamera(const Camera &camera);
+
+    struct {
+      vec3f lower_left;
+      vec3f horizontal;
+      vec3f vertical;
+    } screen;
+    struct {
+      vec3f center;
+      vec3f du;
+      vec3f dv;
+      float radius { 0.f };
+    } lens;
+  };
+    
   class OWLViewer : public QOpenGLWidget, protected QOpenGLFunctions
   {
     Q_OBJECT
 
   public:
     using QOpenGLWidget::QOpenGLWidget;
-
-    OWLViewer() {
-      printf("creating timer...\n");
-      connect(&timer, SIGNAL(timeout()), this, SLOT(update()));
-      timer.start(1);
-    }
-  
-    ~OWLViewer();
 
     // ------------------------------------------------------------------
     // VIEWER LOGIC
@@ -52,7 +69,177 @@ namespace owlQT {
       axis-aligend */
     static vec3f getUpVector(const vec3f &v);
 
-  
+
+    OWLViewer(const std::string &title = "OWL Sample Viewer",
+              const vec2i &initWindowSize=vec2i(1200,800),
+              bool visible=true
+              // ,
+              // const vec3f &cameraInitFrom = vec3f(0,0,-1),
+              // const vec3f &cameraInitAt   = vec3f(0,0,0),
+              // const vec3f &cameraInitUp   = vec3f(0,1,0),
+              // const float worldScale      = 1.f
+              );
+      
+
+    ~OWLViewer();
+
+
+    /*! window notifies us that we got resized */     
+    virtual void resize(const vec2i &newSize);
+
+    void setTitle(const std::string &s);
+
+    /*! gets called whenever the viewer needs us to re-render out widget */
+    virtual void render() {}
+      
+    /*! draw framebuffer using OpenGL */
+    virtual void draw();
+
+    struct ButtonState {
+      bool  isPressed        { false };
+      vec2i posFirstPressed  { -1 };
+      vec2i posLastSeen      { -1 };
+      bool  shiftWhenPressed { false };
+      bool  ctrlWhenPressed  { false };
+      bool  altWhenPressed   { false };
+    };
+
+    ButtonState leftButton;
+    ButtonState rightButton;
+    ButtonState centerButton;
+    vec2i       lastMousePosition { -1,-1 };
+
+    /*! this gets called when the window determines that the mouse
+      got _moved_ to the given position */
+    virtual void mouseMotion(const vec2i &newMousePosition);
+
+    /*! mouse got dragged with left button pressedn, by 'delta'
+      pixels, at last position where */
+    virtual void mouseDragLeft  (const vec2i &where, const vec2i &delta);
+      
+    /*! mouse got dragged with left button pressedn, by 'delta'
+      pixels, at last position where */
+    virtual void mouseDragCenter(const vec2i &where, const vec2i &delta);
+      
+    /*! mouse got dragged with left button pressedn, by 'delta'
+      pixels, at last position where */
+    virtual void mouseDragRight (const vec2i &where, const vec2i &delta);
+      
+    /*! mouse button got either pressed or released at given location */
+    virtual void mouseButtonLeft  (const vec2i &where, bool pressed);
+      
+    /*! mouse button got either pressed or released at given location */
+    virtual void mouseButtonCenter(const vec2i &where, bool pressed);
+      
+    /*! mouse button got either pressed or released at given location */
+    virtual void mouseButtonRight (const vec2i &where, bool pressed);
+
+    /*! this gets called when the user presses a key on the keyboard ... */
+    virtual void key(char key, const vec2i &/*where*/);
+    /*! this gets called when the user presses a 'special' key on
+      the keyboard (cursor keys) ... */
+    virtual void special(int key, int mods, const vec2i &/*where*/);
+
+    /*! set a new window aspect ratio for the camera, update the
+      camera, and notify the app */
+    void setAspect(const float aspect)
+    {
+      camera.setAspect(aspect);
+      updateCamera();
+    }
+
+    /*! set a new orientation for the camera, update the camera, and
+      notify the app */
+    void setCameraOrientation(/* camera origin    : */const vec3f &origin,
+                              /* point of interest: */const vec3f &interest,
+                              /* up-vector        : */const vec3f &up,
+                              /* fovy, in degrees : */float fovyInDegrees);
+
+    void setCameraOptions(float fovy,
+                          float focalDistance);
+
+    /*! this function gets called whenever any camera manipulator
+      updates the camera. gets called AFTER all values have been updated */
+    virtual void cameraChanged() {}
+
+    /*! return currently active window size */
+    vec2i getWindowSize() const { return fbSize; }
+    static vec2i getScreenSize();
+      
+    Camera &getCamera() { return camera; }
+
+    /*! helper function that dumps the current frame buffer in a png
+      file of given name */
+    void screenShot(const std::string &fileName);
+      
+      
+    const SimpleCamera getSimplifiedCamera() const
+    {
+      return SimpleCamera(camera);
+    }
+
+    std::shared_ptr<CameraManipulator> cameraManipulator;
+    std::shared_ptr<CameraManipulator> inspectModeManipulator;
+    std::shared_ptr<CameraManipulator> flyModeManipulator;
+
+    void enableFlyMode();
+    enum RotateMode { POI, Arcball };
+    void enableInspectMode(RotateMode rm,
+                           const box3f &validPoiRange=box3f(),
+                           float minPoiDist=1e-3f,
+                           float maxPoiDist=std::numeric_limits<float>::infinity());
+    void enableInspectMode(const box3f &validPoiRange=box3f(),
+                           float minPoiDist=1e-3f,
+                           float maxPoiDist=std::numeric_limits<float>::infinity());
+    void setWorldScale(const float worldScale)
+    {
+      camera.motionSpeed = worldScale / sqrtf(3.f);
+    }
+
+    /*! re-computes the 'camera' from the 'cameracontrol', and notify
+      app that the camera got changed */
+    void updateCamera();
+
+    void showAndRun();
+      
+    void mouseButton(int button, int action, int mods);
+      
+    /*! the full camera state we are manipulating */
+    Camera camera;
+
+    vec2i getMousePos() const;
+    // inline vec2i getMousePos() const
+    // {
+    //   double x,y;
+    //   glfwGetCursorPos(handle,&x,&y);
+    //   return vec2i((int)x, (int)y);
+    // }
+  private:
+    friend struct CameraManipulator;
+    friend struct CameraInspectMode;
+    friend struct CameraFlyMode;
+
+  protected:
+
+    
+      
+    vec2i    fbSize { 0 };
+
+    GLuint   fbTexture  {0};
+    cudaGraphicsResource_t cuDisplayTexture { 0 };
+    uint32_t *fbPointer { nullptr };
+      
+    /*! the glfw window handle */
+    // GLFWwindow *handle { nullptr };
+    vec2i lastMousePos = { -1,-1 };
+
+    /*! tracks whether we could successfully do cuda resource
+      binding to the GL display texture; if not, we'll have to
+      fall back to a slower path with glTexImage */
+    bool resourceSharingSuccessful;
+
+
+    
     // ------------------------------------------------------------------
     // QT STUFF
     // ------------------------------------------------------------------
